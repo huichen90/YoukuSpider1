0fa2de8f11bb475f02e923eebd74215cd9f9f577
huochen <huichen90>
2018-05-23 16:16:32 +0800

    添加字段

--- youkuspider/youkuspider/items.py
+++ youkuspider/youkuspider/items.py
@@ -11,4 +11,17 @@ import scrapy
 class YoukuspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    keywords = scrapy.Field()
+    tags = scrapy.Field()
+    upload_time = scrapy.Field()
+    url = scrapy.Field()
+    info = scrapy.Field()
+    site_name = scrapy.Field()
+    video_time = scrapy.Field()
+    play_count = scrapy.Field()
+    video_category = scrapy.Field()
+    limit_time = scrapy.Field()
+    task_id = scrapy.Field()
+    start_date = scrapy.Field()
+    end_date = scrapy.Field()
